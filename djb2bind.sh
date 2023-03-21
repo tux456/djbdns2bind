e7f300e5e5a52d819f010b3a07bf9b3381dfd357
tux456 <tux456@gmail.com>
2023-03-21 10:40:00 -0400

    Update djb2bind.sh
    some clean

--- djb2bind.sh
+++ djb2bind.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-if [ -z "$MASTER" ];then MASTER="INSERT_MASTER_DNS.HERE.COM";fi
-if [ -z "$ORIGIN" ];then ORIGIN="null";fi
+if [ -z "$ORIGIN" ];then echo "Need to set ORIGIN env";exit 1;fi
 
+#echo "; ***********************************"
+#echo "; * $ORIGIN"
+#echo "; ***********************************"
+#echo "; "
+if [ -n "$MASTER" ];then
 cat <<fin667
-@     IN     SOA    $MASTER.     hostmaster.$ORIGIN. (
-                    2001062501 ; serial
-                    21600      ; refresh after 6 hours
-                    3600       ; retry after 1 hour
-                    604800     ; expire after 1 week
-                    86400 )    ; minimum TTL of 1 day
+$ORIGIN.  2560   IN     SOA    $MASTER.     hostmaster.$ORIGIN. (
+                    $(date +%s) ; serial
+                    16384      ; refresh 
+                    2048       ; retry
+                    1048576     ; expire 
+                    2560 )    ; minimum TTL of 1 day
 fin667
+if [ -n "$DEFAULT_TTL" ];then echo "\$TTL $DEFAULT_TTL";fi
 
-# echo "\$ORIGIN $ORIGIN"
-if [ -z "$DEFAULT_TTL" ];then DEFAULT_TTL="1h";fi
-echo "\$TTL $DEFAULT_TTL"
-
-if [[ "$ORIGIN" =~ .*".arpa" ]];then
-  ZONETYPE=PTR
-  PTR_ORIGIN=$(echo $(echo $ORIGIN | tr '.' '\n' | tac | xargs |sed 's|[^0-9 ]||g') |tr \  .)
-#  echo PTR_ORIGIN=$PTR_ORIGIN
-else
-  ZONETYPE=DIRECT
 fi
 
 
-# Read each line of the file
+if [[ "$ORIGIN" =~ .*".arpa" ]];then
+  PTR_ORIGIN=$(echo $(echo $ORIGIN | tr '.' '\n' | tac | xargs |sed 's|[^0-9 ]||g') |tr \  .)
+fi
+
+
+
 while read line; do
-#  echo "DJB LINE: $line"
   # Put the first character in the DNS_TYPE variable
   DNS_TYPE=${line:0:1}
   LINE=$(echo "$line" | cut -c2-)
@@ -43,24 +42,28 @@ while read line; do
   OUTZONE=false
   if [ $ZONETYPE == "PTR" ];then
     f2_short=$(echo $f2 |sed "s/$PTR_ORIGIN.//" )
-    if [ "$f2_short" == "$f2" ];then OUTZONE=true;fi
+    f1_short=$(echo $f1 |sed "s/.$PTR_ORIGIN//" )
   elif [ "$ZONETYPE" == "DIRECT" ];then
-    f1_short=$(echo $f1 |sed "s/.$ORIGIN//" )  
-    if [ "$f1_short" == "$f1" ];then OUTZONE=true;fi
+    f1_short=$(echo $f1 |sed "s/.$ORIGIN//" )
+    [ "$f1_short" == "$ORIGIN" ] && f1_short="@"
   fi
- 
- if [ "$DNS_TYPE" == "#" ];then echo ";  $LINE"
-   elif [ "$DNS_TYPE" == "&" ];then echo -e "$(echo $f1 |sed "s/$ORIGIN//" )\tIN MX $f2 $f3"
-   elif [ "$DNS_TYPE" == "." ];then echo -e "$(echo $f1 |sed "s/$ORIGIN//" )\tIN NS $(echo $f3.|sed 's/[.][.]/./g')"
+
+ if [ "$DNS_TYPE" == "#" ];then [ -z "$SILENT" ] && echo ";  $line"
+#   elif [ "$DNS_TYPE" == "&" ];then echo -e "$(echo $f1 |sed "s/$ORIGIN//" ) IN MX $f2 $f3"
+   elif [ "$DNS_TYPE" == "." -a "$f1_short" == "@" ];then echo -e "@ 259200 IN NS $(echo $f3.|sed 's/[.][.]/./g')"
+   elif [ "$DNS_TYPE" == "." -a "$f1_short" == "$f1" ];then echo -e "@ 259200 IN NS $(echo $f3.|sed 's/[.][.]/./g')"
+   elif [ "$DNS_TYPE" == "@" -a "$f1_short" == "@" ];then echo -e "@ IN MX $f4 $(echo $f3.|sed 's/[.][.]/./g')"
+   elif [ "$DNS_TYPE" == "@" ];then echo -e "@ IN MX $f4 $(echo $f3.|sed 's/[.][.]/./g')"
+   elif [ "$DNS_TYPE" == "@" ];then echo -e "$f1 IN MX $f4 $(echo $f3.|sed 's/[.][.]/./g')"
    elif [ "$DNS_TYPE" == "+" -a "$ZONETYPE" == "PTR" ];then echo ";"
-   elif [ "$OUTZONE" == "true" ];then echo ";  OUTZONE:  $line"
-   elif [ "$DNS_TYPE" == "C" ];then echo -e "$f1_short\tIN CNAME $f2"
-   elif [ "$DNS_TYPE" == "'" ];then echo -e "$f1_short\tIN TXT \"$f2\" "
-   elif [ "$DNS_TYPE" == "+" -a "$ZONETYPE" != "PTR" ];then echo -e "$f1_short\tIN A $f2"
-   elif [ "$DNS_TYPE" == "=" -a "$ZONETYPE" != "PTR" ];then echo -e "$f1_short\tIN A $f2"
-   elif [ "$DNS_TYPE" == "=" -a "$ZONETYPE" == "PTR" ];then echo -e "$f2_short\tIN PTR $f1"
+   elif [ "$DNS_TYPE" == "C" -a "$ZONETYPE" != "PTR" -a "$f1_short" != "$f1" ];then echo -e "$f1_short IN CNAME $f2."
+   elif [ "$DNS_TYPE" == "'" -a "$f1_short" == "@"  -a "$ZONETYPE" != "PTR" ];then echo -e "$f1_short $f3 IN TXT \"$f2\" "
+   elif [ "$DNS_TYPE" == "'" -a "$ZONETYPE" != "PTR" ];then echo -e "$f1_short $f3 IN TXT \"$f2\" "
+   elif [ "$DNS_TYPE" == "+" -a "$f1_short" != "$f1" ];then echo -e "$f1_short IN A $f2"
+   elif [ "$DNS_TYPE" == "=" -a "$ZONETYPE" != "PTR" -a "$f1_short" != "$f1" ];then echo -e "$f1_short IN A $f2"
+   elif [ "$DNS_TYPE" == "=" -a "$ZONETYPE" == "PTR" -a "$f2_short" != "$f2" ];then echo -e "$f2_short IN PTR $f1."
 
    elif [ "$line" == "" ];then echo ""
-   else echo ";  CONVERSION NOT FOUND:  $line"
+   else [ -z "$SILENT" ] && echo ";  CONVERSION NOT FOUND:  $line"
   fi
 done < $1
